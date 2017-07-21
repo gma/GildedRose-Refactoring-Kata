9930d1f7bff6f25bb91c1c58853128852f61567a
Graham Ashton <graham@effectif.com>
2017-07-21 08:17:08 +0100

    Start refactoring with 'extract method'

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -7,27 +7,9 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+        decrease_quality(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+        increase_quality(item)
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
@@ -35,11 +17,7 @@ class GildedRose
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
+            decrease_quality(item)
           else
             item.quality = item.quality - item.quality
           end
@@ -48,6 +26,33 @@ class GildedRose
             item.quality = item.quality + 1
           end
         end
+      end
+    end
+  end
+
+  def increase_quality(item)
+    if item.quality < 50
+      item.quality = item.quality + 1
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.sell_in < 11
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+        if item.sell_in < 6
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
+    end
+  end
+
+  def decrease_quality(item)
+    if item.quality > 0
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        item.quality = item.quality - 1
+        item.quality = item.quality - 1 if item.name == "Conjured"
       end
     end
   end
--- ruby/gilded_rose_tests.rb
+++ ruby/gilded_rose_tests.rb
@@ -2,11 +2,83 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 require 'test/unit'
 
 class TestUntitled < Test::Unit::TestCase
-
-  def test_foo
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    assert_equal items[0].name, "fixme"
+  def update_quality(item)
+    GildedRose.new([item]).update_quality()
   end
 
+  def test_aged_brie_quality_increases
+    quality = 1
+    brie = Item.new('Aged Brie', 1, quality)
+    update_quality(brie)
+    assert_equal quality + 1, brie.quality
+  end
+
+  def test_backstage_pass_quality_increases
+    quality = 1
+    backstage = Item.new('Backstage passes to a TAFKAL80ETC concert', 1, quality)
+    update_quality(backstage)
+    assert_equal quality + 3, backstage.quality
+  end
+
+  def test_conjured_item_degrades_twice_as_fast
+    normal = Item.new('Normal', 1, 2)
+    conjured = Item.new('Conjured', 1, 2)
+    update_quality(conjured)
+    2.times { update_quality(normal) }
+    assert_equal normal.quality, conjured.quality
+  end
+
+  def test_outdated_conjured_item_degrades_twice_as_fast
+    normal = Item.new('Normal', -1, 4)
+    conjured = Item.new('Conjured', -1, 4)
+    update_quality(conjured)
+    2.times { update_quality(normal) }
+    assert_equal normal.quality, conjured.quality
+  end
+
+  def test_normal_items_go_out_of_date
+    sell_in = 1
+    normal = Item.new('Normal', sell_in, 0)
+    update_quality(normal)
+    assert_equal sell_in - 1, normal.sell_in
+  end
+
+  def test_sulfuras_does_not_go_out_of_date
+    sell_in = 2
+    sulfuras = Item.new('Sulfuras, Hand of Ragnaros', sell_in, 2)
+    update_quality(sulfuras)
+    assert_equal sell_in, sulfuras.sell_in
+  end
+
+  def test_outdated_normal_item_degrades_quality
+    quality = 4
+    sell_in = -1
+    normal = Item.new('Normal', sell_in, quality)
+    update_quality(normal)
+    assert_equal quality - 2, normal.quality
+  end
+
+  def test_outdated_brie_increase_quality
+    quality = 2
+    sell_in = -1
+    brie = Item.new('Aged Brie', sell_in, quality)
+    update_quality(brie)
+    assert_equal quality + 2, brie.quality
+  end
+
+  def test_outdated_pass_quality_reduced_to_zero
+    quality = 2
+    sell_in = -1
+    backstage_pass = Item.new('Backstage passes to a TAFKAL80ETC concert', sell_in, quality)
+    update_quality(backstage_pass)
+    assert_equal 0, backstage_pass.quality
+  end
+
+  def test_outdated_sulfuras_quality_doesnt_change
+    quality = 2
+    sell_in = -1
+    sulfuras = Item.new('Sulfuras, Hand of Ragnaros', sell_in, quality)
+    update_quality(sulfuras)
+    assert_equal quality, sulfuras.quality
+  end
 end
